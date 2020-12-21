c234ef53eb4f23a2dbfa617b5600f15d0047a65c
Nam Nguyen <nguyenhainam.fetel@gmail.com>
2020-12-21 10:27:18 +0700

    ID-0 | Fix errors with get_paginator

--- iam/roles_can_assume.py
+++ iam/roles_can_assume.py
@@ -1,4 +1,4 @@
-import boto3, re, sys
+import boto3, re, sys, itertools
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -6,20 +6,25 @@ def get_statements(roles, value_to_find):
     temp = []
     for order, role in enumerate(roles):
         for state in role['AssumeRolePolicyDocument']['Statement']:
-            if  'Principal' in state and 'AWS' in state['Principal']:
-                temp.append(
-                    (roles[order]['Arn'], roles[order]['RoleName'], state['Principal']['AWS'])
-                )
+            for value in state['Principal'].values():
+                if isinstance(value, str):
+                    temp.append(
+                        (roles[order]['Arn'], roles[order]['RoleName'], list(value))
+                    )
+                elif isinstance(value, list):
+                    temp.append(
+                        (roles[order]['Arn'], roles[order]['RoleName'], value)
+                    )
     final = []
-    for first,key,value in temp:
-        if isinstance(value, str):
-            if re.search(value_to_find, value):
-                final.append(dict(Arn=first, RoleName=key))
-        elif isinstance(value, list):
-            for item in value:
-                if re.search(value_to_find, item):
-                    final.append(dict(Arn=first, RoleName=key))
-
+    for arn, role, principals in temp:
+        for item in principals:
+            if re.search(value_to_find, item):
+                final.append(
+                        {
+                            'Arn': arn,
+                            'RoleName':role
+                        }
+                    )
     return final
 
 if __name__ == "__main__":
