d466864c7b82d9abacddb93afa6be6f70bc7a9ea
Nam Nguyen <nguyenhainam.fetel@gmail.com>
2020-12-27 00:11:56 +0700

    ID-3 | Add a new Ec2 command

---
+++ ec2/security_groups_by_name.py
@@ -0,0 +1,39 @@
+import boto3, re, click, json, datetime
+from base import Ec2
+
+def convert_time_to_string(value):
+    if isinstance(value, datetime.datetime):
+        return value.__str__()
+
+@click.command()
+@click.option("--name", help="The name of the SecurityGroup you want to find")
+@click.option('--filter-keys', help="A list of key to extract only, seperate by a comma")
+def by_name(name, filter_keys):
+    ec2 = Ec2("ap-southeast-1", "default")
+    """
+        This command lists all SecurityGroups whose name matched your input
+    """
+    origin = []
+    for sec in ec2.security_groups:
+        if re.search(name, sec['GroupName']):
+            origin.append(sec)
+
+    if filter_keys:
+        final = []
+        return_items = filter_keys.split(',')
+        for unit in origin:
+            _dict = {}
+            for key,value in unit.items():
+                if key in return_items:
+                    _dict[key] = value
+            if _dict:
+                final.append(_dict)
+        final = json.dumps(final, sort_keys=True, indent=2, default = convert_time_to_string,
+                )
+        click.echo(final)
+    else:
+        origin = json.dumps(origin, sort_keys=True, indent=2, default = convert_time_to_string)
+        click.echo(origin)
+
+if __name__ == "__main__":
+    by_name()
